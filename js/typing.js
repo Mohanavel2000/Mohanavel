af628ae72eb8f2b45036acc3314fbd2ee9a6bf4f
Mohanavel R <vemohan15@gmail.com>
2020-08-19 17:03:10 +0530

    one

---
+++ js/typing.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing', {
+    strings: ["Hi, Welcome to my Portfolio.", "I Hope you enjoy it."],
+    typeSpeed: 80,
+    backSpeed:20,
+    loop:true,
+    loopCount:false
+    });
